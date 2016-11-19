e33cd5b095a56d80c17935981e7ea47fc7fcaea4
gautam <gsadaran@asu.edu>
2016-11-19 15:53:55 -0700

    load_distributer added

--- NFV/load_distributer.cpp
+++ NFV/load_distributer.cpp
@@ -1,17 +1,76 @@
-struct Server{
-	int cores;
-	vector<double> net_stats;
-	vector<double> cpu_loads;
-	vector<double> tps_parts;
-	int load_percentage;
-	int file_start_index;
-	int file_end_index;
+#include <math.h>
+#include <cstdlib>
+#include <vector>
+#include <string>
 
+#include "load_distributer.h"
+
+using namespace std;
+
+Server::Server(){
+	Server::tps_load = Server::getMax(Server::tps_parts);
+	Server::net_out = Server::net_stats[1];
+	Server::getPidWeight();	
 }
 
-int main{
-	for(i=1;i<l.cpu_loads.size();i++){
-		if(cpu_loads[i] > 0.85)
+void getWeightedLoad(Server &s){
+	
+	if(s.cpu_load > MAX_CPU_LOAD){
+		s.load_percentage = 0;
+		return;
+	}
+	else{
+		if(s.net_out > MAX_NET_LOAD){
+			s.load_percentage = 0;
+			return;
+		}
+		else{
+			if(s.tps_load > MAX_TPS_LOAD){
+				s.load_percentage = 0;
+				return;
+			}
+			// normalize parameters
+			s.normalize();
+			// allocate load %
+			s.load_percentage = 0.4*s.tps_load + 0.4*s.net_out + 0.2*s.cpu_load;
+		}
+	} 
+				
+}
+
+void getFileRange(Server &s, int &filecounter){
+	s.file_start_index = filecounter;
+	s.file_end_index = s.file_start_index + s.load_percentage;
+	filecounter = s.file_end_index + 1;
+}
+
+
+int main(){
+
+	Server s[3];
+	int sum = 0, i, sum2 = 0;
+	int filecounter = 0;
+	for(i = 0; i < 3; i++){
+		getWeightedLoad(s[i]);
+		sum += s[i].load_percentage;
+	}
+
+	for(i = 0; i < 3; i++){
+		s[i].load_percentage = sum/s[i].load_percentage;
+		sum2 += s[i].load_percentage;
+	}
+
+	for(i = 0; i < 3; i++){
+		s[i].load_percentage /= sum2;
+		s[i].load_percentage = round(s[i].file_size*s[i].load_percentage);
+	}
+
+	for(i = 0; i < 3; i++){
+		getFileRange(s[i],filecounter);
 	}
 
 }
+
+
+
+
---
+++ NFV/load_distributer.h
@@ -0,0 +1,54 @@
+
+using namespace std;
+
+const double MAX_CPU_LOAD = 0.85;
+const double MAX_TPS_LOAD = 70;  
+const double MAX_NET_LOAD = 10000; //10MB/s
+const int NO_OF_SERVERS = 3;
+
+class Server{
+	public :
+	
+	double cpu_load;
+	double tps_load;
+	double net_out;
+	
+	int cores;
+
+	double load_percentage;
+	string interface;
+	int file_size;
+	int file_start_index;
+	int file_end_index;
+
+	Server();
+
+	void normalize(){
+		tps_load /= MAX_TPS_LOAD;
+		net_out /= MAX_NET_LOAD;
+		for(int i = 0; i<3; i++){
+			cpu_loads[i] /= MAX_CPU_LOAD;
+		}
+		
+	}
+
+	private :
+	vector<double> cpu_loads;
+	vector<double> tps_parts;
+	vector<double> net_stats;
+
+	double getMax(vector<double> v){
+		int max = 0;
+		for(int i = 0; i< v.size(); i++){
+			if(v[i] > max)
+				max = v[i];
+		}
+		return max;
+	}
+
+	
+	void getPidWeight(){
+		cpu_load = cpu_loads[0]*0.8 + cpu_loads[1]*0.1 + (cpu_loads[0]-cpu_loads[1])*0.1;
+	}
+
+};
